8b8ea417f28e27ba3e30cca9411b8bbd2307b9ee
Trace Lewis <tracelewis@kevins-iMac.localdomain>
2024-06-28 12:43:30 -0500

    index.html

---
+++ index.js
@@ -0,0 +1,12 @@
+/*pseudo code
+linking my js to HTML 
+the actualy combination numbers 1, 2 and 3 
+stringing my 3 numbers together so they pop up when running alert 
+alert tag to show the alert message from my string 
+*/
+
+const combination1 = 30-20;
+const combination2 = 30+10;
+const combination3 = 30+9;
+let string = `You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${combination1}-${combination2}-${combination3}`
+alert (string)
